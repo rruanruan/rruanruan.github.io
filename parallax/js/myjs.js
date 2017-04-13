0abfe1a1f3e8c75ee89b45e447005c0087d9d5ff
rruanruan <rruanruan@163.com>
2017-04-14 01:04:02 +0800

    读取元素高度有误bug修正

--- parallax/js/myjs.js
+++ parallax/js/myjs.js
@@ -6,9 +6,10 @@ $(function(){
 		zindex:100,
 		background:'rgba(0,0,0,0.3)'
 	});
-	skrollr.init({
-		smoothScrollingDuration: 5000,
-		constants: {
+	setTimeout(function(){
+		skrollr.init({
+			smoothScrollingDuration: 5000,
+			constants: {
 				valinit: function() {
 					return $(window).height()/2;
 				},
@@ -23,7 +24,8 @@ $(function(){
 					return 	$('.path4').position().top -($(window).height()*0.5);
 				}
 			}
-	});
+		});
+	},20);
 	$(window).on('scroll',function(){
 		if($(window).scrollTop()>$('header').position().top){
 			$('nav').css('position','fixed');
@@ -37,22 +39,22 @@ $(function(){
 		if(windowH>600){
 			var h = Math.ceil($(window).innerHeight()/145)*145;
 			$('.welcome').height(h);
-			
+			console.log(h,'aa');
 		}else{
 			$('.welcome').height(500);
 		}
 		$('.welcome h1').css('marginTop',-$('.welcome h1').height()/2);
 	})
-	// $(window).on('mousemove',function(ev){
-	// 	var lefta = ($(window).innerWidth()-ev.pageX);
-	// 	var thisPos = $('.text_shadow').offset().top;
-	// 	var thisCenter = $('.text_shadow').height() / 2;
-	// 	var thisPosCenter = ev.pageY - (thisPos + thisCenter);
-	// 	var thisPosCenter = ev.pageY - (thisPos + thisCenter);
-	// 	TweenMax.to($(".bg"), .7, {css: {left: -lefta*0.015}});
-	// 	TweenMax.to($(".cuadros1"), .7, {css: {left: -lefta*0.035}});
-	// 	TweenMax.to($(".cuadros2"), .7, {css: {left: -lefta*0.025}});
-	// 	TweenMax.to($('.text_shadow'), .5, {css:{textShadow:'' + -((lefta * 0.02)) + 'px ' + -((thisPosCenter * 0.02)) + 'px 0 rgba(24,230,152,0.5)'}});
+	$(window).on('mousemove',function(ev){
+		var lefta = ($(window).innerWidth()-ev.pageX);
+		var thisPos = $('.text_shadow').offset().top;
+		var thisCenter = $('.text_shadow').height() / 2;
+		var thisPosCenter = ev.pageY - (thisPos + thisCenter);
+		var thisPosCenter = ev.pageY - (thisPos + thisCenter);
+		TweenMax.to($(".bg"), .7, {css: {left: -lefta*0.015}});
+		TweenMax.to($(".cuadros1"), .7, {css: {left: -lefta*0.035}});
+		TweenMax.to($(".cuadros2"), .7, {css: {left: -lefta*0.025}});
+		TweenMax.to($('.text_shadow'), .5, {css:{textShadow:'' + -((lefta * 0.02)) + 'px ' + -((thisPosCenter * 0.02)) + 'px 0 rgba(24,230,152,0.5)'}});
 
 	// 	// move($('#boxx'),'left',left,300);
 	// 	//move($('.cuadros2'),'left',100,1000);
